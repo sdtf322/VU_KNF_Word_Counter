340e8d616006a9787ce52433f7bffec227e0658b
Olegs Pliska <olegpliska1@gmail.com>
2022-11-17 10:51:48 +0200

    added test classes

---
+++ app/src/test/java/com/example/vu_knf_word_counter/CharCounterUtilTest.kt
@@ -0,0 +1,15 @@
+package com.example.vu_knf_word_counter
+
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class CharCounterUtilTest {
+
+    @Test
+    fun whenInputIsValid() {
+
+    }
+}
---
+++ app/src/test/java/com/example/vu_knf_word_counter/WordCounterUtilTest.kt
@@ -0,0 +1,5 @@
+package com.example.vu_knf_word_counter
+
+import org.junit.Assert.*
+
+class WordCounterUtilTest
