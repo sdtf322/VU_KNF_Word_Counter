e2b8cbdacc61068777aa4366c0b6da0745732585
Olegs Pliska <olegpliska1@gmail.com>
2022-11-17 12:23:44 +0200

    implemented char counter unit tests

--- app/src/test/java/com/example/vu_knf_word_counter/CharCounterUtilTest.kt
+++ app/src/test/java/com/example/vu_knf_word_counter/CharCounterUtilTest.kt
@@ -1,5 +1,6 @@
 package com.example.vu_knf_word_counter
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
@@ -7,8 +8,26 @@ import org.junit.runners.JUnit4
 @RunWith(JUnit4::class)
 class CharCounterUtilTest {
 
+    private val charCounter by lazy { CharCounterUtil() }
+
     @Test
     fun whenInputIsValid() {
-        
+
+        val testString = "Let's count all chars!!!"
+        val expectedString = "24 Chars"
+        val result = charCounter.countChars(testString)
+
+        assertEquals(expectedString, result)
     }
+
+    @Test
+    fun whenInputIsEmpty() {
+
+        val testString = ""
+        val expectedString = "0 Chars"
+        val result = charCounter.countChars(testString)
+
+        assertEquals(expectedString, result)
+    }
+
 }
