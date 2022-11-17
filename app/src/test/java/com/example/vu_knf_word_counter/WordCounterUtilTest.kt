c51682eaed13bcdbaa6880fd95a1f6f3c0e81ed8
Olegs Pliska <olegpliska1@gmail.com>
2022-11-17 12:32:52 +0200

    implemented word counter unit tests

--- app/src/test/java/com/example/vu_knf_word_counter/WordCounterUtilTest.kt
+++ app/src/test/java/com/example/vu_knf_word_counter/WordCounterUtilTest.kt
@@ -1,5 +1,29 @@
 package com.example.vu_knf_word_counter
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 
-class WordCounterUtilTest
+@RunWith(JUnit4::class)
+class WordCounterUtilTest {
+
+    private val wordCounter by lazy { WordCounterUtil() }
+
+    @Test
+    fun whenInputIsValidWithManySpaces() {
+        val testInput = "Hello     world      space x"
+        val expectedResult = "4 Words"
+        val result = wordCounter.countWords(testInput)
+
+        assertEquals(expectedResult, result)
+    }
+    @Test
+    fun whenInputIsEmpty() {
+        val testInput = ""
+        val expectedResult = "0 Words"
+        val result = wordCounter.countWords(testInput)
+
+        assertEquals(expectedResult, result)
+    }
+}
